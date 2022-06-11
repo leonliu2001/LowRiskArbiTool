11f42f0e16564c187b3a2c6092e9378a42bb2ac0
leonliu2001 <49064592+leonliu2001@users.noreply.github.com>
2022-06-11 15:47:24 +0800

    Update sina_tick.py

--- sina_tick.py
+++ sina_tick.py
@@ -11,7 +11,7 @@ class SinaTick:
     def __init__(self, code_no: str):
         self.code_no = code_no
 
-    def get_stock(self):
+        def get_stock(self):
         # 取股票当前数据
         url = "http://hq.sinajs.cn/list=" + self.code_no
         headers = {'referer': 'https://finance.sina.com.cn/'}
@@ -29,8 +29,28 @@ class SinaTick:
         opti_data = option_info[13:-3].replace('="', ',').split(',')
         return opti_data
 
+
+    def get_cff_future(self):
+        # 取中金所期货当前数据，代码格式如IC2207
+        url = "http://hq.sinajs.cn/list=CFF_RE_" + self.code_no
+        headers = {'referer': 'https://finance.sina.com.cn/'}
+        page = requests.get(url, headers=headers)
+        future_info = page.text
+        future_data = future_info[18:-3].replace('="', ',').split(',')
+        return future_data
+
+    def get_merch_future(self):
+        # 取商品期货当前数据，代码格式如M2207
+        url = "http://hq.sinajs.cn/list=" + self.code_no
+        headers = {'referer': 'https://finance.sina.com.cn/'}
+        page = requests.get(url, headers=headers)
+        merch_info = page.text
+        merch_data = merch_info[15:-3].replace('="', ',').split(',')
+        return merch_data
+
+
     def get_etf_opti(self):
-        # 取etf期权当前数据
+        # 取etf期权当前数据，代码格式如10003707，为上交所公布的合约编号
         url = "http://hq.sinajs.cn/list=" + self.code_no
         headers = {'referer': 'https://finance.sina.com.cn/'}
         page = requests.get(url, headers=headers)
