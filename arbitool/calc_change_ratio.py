2a5831301a9a71169f62f1d3c284444599ab5ecd
leonliu2001 <49064592+leonliu2001@users.noreply.github.com>
2022-06-16 16:52:15 +0800

    Update calc_change_ratio.py

--- arbitool/calc_change_ratio.py
+++ arbitool/calc_change_ratio.py
@@ -4,49 +4,41 @@ from sina_tick import SinaTick
 csv_file = './json/cbondlist.csv'
 csv_data = pd.read_csv(csv_file, low_memory=False, converters={'stcode': str})
 csv_df = pd.DataFrame(csv_data)
-
 stock_keys = list(csv_df['cbcode'])
-m = len(csv_df.index) # m为转债对照表的行数
+m = len(csv_df.index)
 n = 0
 while n < m:
     CbondCode = csv_df.loc[n, 'cbcode']
     StockCode = csv_df.loc[n, 'stcode']
     ChangePrice = csv_df.loc[n, 'cprice']
-
-    if str(CbondCode).startswith('11'):  # 挑选上海交易所的转债
-        print("转债代码", str(CbondCode))
+    if str(CbondCode).startswith('11'):  # 挑选上海交易所的股票
         CbondCodeSh = 'sh' + str(CbondCode)  # 增加sh标识
         CbondTicksSh = SinaTick(CbondCodeSh)
-        CbondTickSh = CbondTicksSh.get_stock()  # 抽取转债当前价格
-        print("转债现价", CbondTickSh[4])
+        CbondTickSh = CbondTicksSh.get_stock()  # 抽取股票当前价格
         cp = float(CbondTickSh[4])
-
-        if str(StockCode).startswith('60'): # 挑选上海交易所的股票
-            print("股票代码", str(StockCode))
+        if str(StockCode).startswith('60'):
             StockCodeSh = 'sh' + str(StockCode)  # 增加sh标识
             StockTicksSh = SinaTick(StockCodeSh)
             StockTickSh = StockTicksSh.get_stock()  # 抽取股票当前价格
-            print("股票现价", StockTickSh[4])
             sp = float(StockTickSh[4])
-            Discount = (cp - ((100 / float(ChangePrice)) * sp)) / (sp * (100 / float(ChangePrice)))  # 计算折溢价
-            print("折溢价为：", Discount)
+            Discount = (cp - ((100 / float(ChangePrice)) * sp)) / (sp * (100 / float(ChangePrice)))
+            if Discount < 0.02: # 仅输出折溢价率小于2%的转债
+                print("转债代码", str(CbondCode), "转债现价", CbondTickSh[4], "股票代码", str(StockCode), "股票现价", StockTickSh[4])
+                print("折溢价为：", Discount)
 
     if str(CbondCode).startswith('12'):  # 挑选深圳交易所的转债
-        print("转债代码", str(CbondCode))
         CbondCodeSz = 'sz' + str(CbondCode)  # 增加sz标识
-        CbondticksSz = SinaTick(CbondCodeSz)
-        CbondTickSz = CbondticksSz.get_stock()
-        print("转债现价", CbondTickSz[4])
+        CbondTicksSz = SinaTick(CbondCodeSz)
+        CbondTickSz = CbondTicksSz.get_stock()
         cp = float(CbondTickSz[4])
-
         if str(StockCode).startswith(('00', '30')):  # 挑选深圳交易所的股票
-            print("股票代码", str(StockCode))
             StockCodeSz = 'sz' + str(StockCode)  # 增加sz标识
             StockTicksSz = SinaTick(StockCodeSz)
-            StockCodeSz = StockTicksSz.get_stock() # 抽取股票当前价格
-            print("股票现价", StockCodeSz[4])
+            StockCodeSz = StockTicksSz.get_stock()
             sp = float(StockCodeSz[4])
-            Discount = (cp - ((100 / float(ChangePrice)) * sp)) / (sp * (100 / float(ChangePrice))) # 计算折溢价
-            print("折溢价为：", Discount)
+            Discount = (cp - ((100 / float(ChangePrice)) * sp)) / (sp * (100 / float(ChangePrice)))
+            if Discount < 0.02: # 仅输出折溢价率小于2%的转债
+                print("转债代码", str(CbondCode), "转债现价", CbondTickSz[4], "股票代码", str(StockCode), "股票现价", StockCodeSz[4] )
+                print("折溢价为：", Discount)
 
     n = n+1
